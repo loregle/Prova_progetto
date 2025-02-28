79a20fb6b9f7a022cb0b04420b8ab8a54e9d7689
loregle <lorenzabonfantiposta@gmail.com>
2025-02-28 13:01:30 +0100

    Initial commit

---
+++ PassoUpwind.m
@@ -0,0 +1,26 @@
+function f = PassoUpwind(L,Nu,Tmax,CFL,w,f_0)
+% f_0 deve essere un vettore che mi dà le condizoni iniziali del ciclo che
+% voglo mettere in atto.
+
+% Discretizzazione spaziale
+du = 2*L / Nu;
+uu = linspace(-L, L, Nu);
+
+dt = CFL * (du /max(abs(w(uu))));
+Nt = ceil(Tmax / dt);  % Numero di passi temporali
+dt = Tmax / Nt;        % Ricalcolo per adattamento
+
+f=f_0;% Evoluzione nel tempo con schema UDS
+%Passo Upwind
+f_new = f;  % Array temporaneo per aggiornamento
+for j = 2:Nu-1
+    if uu(j)>-1 % questo evita di perdere massa numerica
+        % Flusso numerico per w > 0
+        g_jm12 = w(uu(j)) * f(j-1);     % Flusso tra j-1 e j
+        g_jp12 = w(uu(j)) * f(j);   % Flusso tra j e j+1
+        % Aggiornamento della soluzione
+        f_new(j) = f(j) - (dt/du) * (g_jp12 - g_jm12); % qui moltiplicare per dt/2 invece che per dt?
+    end
+end
+% Aggiorna la soluzione
+f = f_new;
---
+++ main.m
@@ -0,0 +1,62 @@
+clearvars; close all;clc
+% particelle per MonteCarlo
+format short;
+N_class = [441148; 5666380; 5962570; 6570438; 8061698; 9619516; 7964818; 6141544; 4543122]; %numero popolazione per ogni fascia d'età
+%N = sum(N_class);
+
+M =  [19.2 4.8 3.0 7.1 3.7 3.1 2.3 1.4 1.4;
+    4.8 42.4 6.4 5.4 7.5 5.0 1.8 1.7 1.7;
+    3.0 6.4 20.7 9.2 7.1 6.3 2.0 0.9 0.9;
+    7.1 5.4 9.2 16.9 10.1 6.8 3.4 1.5 1.5;
+    3.7 7.5 7.1 10.1 13.1 7.4 2.6 2.1 2.1;
+    3.1 5.0 6.3 6.8 7.4 10.4 3.5 1.8 1.8;
+    2.3 1.8 2.0 3.4 2.6 3.5 7.5 3.2 3.2;
+    1.4 1.7 0.9 1.5 2.1 1.8 3.2 7.2 7.2;
+    1.4 1.7 0.9 1.5 2.1 1.8 3.2 7.2 7.2];
+
+L = 10;
+Nu   = 1000;                            % Numero di punti spaziali
+Tmax = 50;                            % Tempo massimo di simulazione
+CFL  = 0.9;                            % Numero di Courant-Friedrichs-Lewyc
+w    = @(u) 1;
+% condizione iniziale, matrice in cui ogni colonna rappresenta una fascia
+% d'età
+U0 = zeros(max(N_class),numel(N_class));
+for N_c = 1:numel(N_class)
+   U0(1:N_class(N_c),N_c) = -10+9.01*rand(N_class(N_c),1); % ho ridotto il numero di infetti iniziali (da 9.1 a 9.01)
+end
+
+% parametri
+beta   = 8.45e-9;
+gamma  = 0.24;
+for t = 1:100
+    for N_c = 1:numel(N_class)
+        % primo passo
+        [f_new_tilda,U,n,edges] = MonteCarlo(U0(1:N_class(N_c),N_c),beta,gamma,N_class(N_c),M(:,N_c),U0);
+        % secondo passo
+        f_new = PassoUpwind(L,n,Tmax,CFL,w,f_new_tilda);
+        % aggiornamento
+        U0(1:N_class(N_c),N_c) = U;
+        S = sum(f_new(1:find(edges==-1)));
+        if isempty(find(edges==1, 1))
+            I = sum(f_new(find(edges==-1):end));
+            R = 0;
+        else
+            r = find(edges==1);
+            if r>n
+                r = n;
+            end
+            I = sum(f_new(find(edges==-1):r));
+            R = sum(f_new(r:end));
+        end
+        T = table(t, S, I, R, 'RowNames', {'Value'}, 'VariableNames', {'Time_Step', 'Susceptible', 'Infected', 'Removed'});
+
+        fprintf('Class %d\n', N_c);
+        disp(rows2vars(T));
+ 
+        % plot della distribuzione della classe N_c
+        plot(edges(1:end-1),f_new)
+        legend(sprintf('Distribution plot of class %d', N_c),'Location','best')
+    end
+    pause
+end
