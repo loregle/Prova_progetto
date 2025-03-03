cfea77b6ba4f3fa8eb209da3a90448df5951472c
loregle <lorenzabonfantiposta@gmail.com>
2025-03-03 21:47:25 +0100

    Add files via upload
    update

--- MonteCarlo.m
+++ MonteCarlo.m
@@ -1,4 +1,4 @@
-function [f,U,bins,edges] = MonteCarlo(U0M,beta,gamma,N_c,M) % U0M mi serve per avere la matrice di partenza
+function [f,U,bins,edges] = MonteCarlo(n,U0M,beta,gamma,N_c,M) % U0M mi serve per avere la matrice di partenza
 % set(0,'DefaultTextInterpreter','latex')
 rng(1)
 
@@ -14,22 +14,36 @@ for j=1:size(U0M,2) % chiedo un ciclo su tutte le classi
     pp      = min(numel(U1),numel(U2));
     Theta_b = binornd(1,beta,pp,1);
     Theta_g = binornd(1,gamma,pp,1);
-    Theta_c=binornd(1,M(j)/no,pp,1);
-    for p = 1:pp
+    Theta_c=binornd(1,1,pp,1);
 
+    if N_c==7
+            Theta_c(1)=1; %altrimenti l'epidemia non parte
+            Theta_c(2)=1;
+    end
+
+    %debug
+    disp(['N_c = ', num2str(N_c), ' - Popolazione coinvolta: ', num2str(pp)]);
+    disp(['Theta_c attivi: ', num2str(sum(Theta_c)), '/', num2str(pp)]);
+    disp(['Theta_b attivi: ', num2str(sum(Theta_b)), '/', num2str(pp)]);
+    disp(['Theta_g attivi: ', num2str(sum(Theta_g)), '/', num2str(pp)]);
+
+
+    for p = 1:pp
+        
         Theta_b(p)=Theta_b(p)*(U1(p)<-1)*(-1<U2(p))*(U2(p)<1); %mi assicuro che le interazioni ci siano tra le classi giuste
         Theta_g(p)=Theta_g(p)*(-1<U1(p))*(U1(p)<1)*(-1<U2(p))*(U2(p)<1);
-
+        
 
         if Theta_c(p)==0
-                %U1new(p)=U1(p);
-                %U2new(p)=U2(p);
+                U1new(p)=U1(p);
+                U2new(p)=U2(p);
         else
             if (U1(p)<=-1) && (abs(U2(p))<=1) % interazione S-I
                 UU1      = (U1(p)+2); % stati post interazione
                 UU2      = U2(p);
                 U1new(p) = (1-Theta_b(p)).*U1(p) + Theta_b(p).*(UU1); % aggiornamento temporale degli stati
                 U2new(p) = (1-Theta_b(p)).*U2(p) + Theta_b(p).*(UU2);
+                
             elseif (abs(U1(p))<=1) && (abs(U2(p))<=1) % interazione I-I
                 UU1      = (U1(p)+2); % stati post interazione
                 UU2      = U2(p);
@@ -44,6 +58,7 @@ for j=1:size(U0M,2) % chiedo un ciclo su tutte le classi
 end
 
 U = U1new; % l'interazione modifica solo la classe i
+disp(['U (stati aggiornati) min/max: ', num2str(min(U)), ' / ', num2str(max(U))]);
 
 h = histogram(U,'Visible','off');
 % ricavo la distribuzione prendendo i valori degli istogrammi
--- main.m
+++ main.m
@@ -17,40 +17,43 @@ M =  [19.2 4.8 3.0 7.1 3.7 3.1 2.3 1.4 1.4;
 L    = 100;
 Nu   = 1000;                            % Numero di punti spaziali
 Tmax = 50;                            % Tempo massimo di simulazione
-Tfin = 30;
+Tfin = 15;
 CFL  = 0.9;                            % Numero di Courant-Friedrichs-Lewy
 % condizione iniziale, matrice in cui ogni colonna rappresenta una fascia
 % d'età
 U0 = nan(max(N_class),numel(N_class));
 for N_c = 1:numel(N_class)
-    U0(1:N_class(N_c),N_c) = -L+(L-1.01)*rand(N_class(N_c),1); % ho ridotto il numero di infetti iniziali (da 9.1 a 9.01)
+    U0(1:N_class(N_c),N_c) = -1.5+(-1.01+1.5)*rand(N_class(N_c),1); % ho ridotto il numero di infetti iniziali (da 9.1 a 9.01)
 end
-%turisti
-U0(1,7)=-0.5;
-U0(2,7)=-0.5;
+
+%Infetti il 29 febbraio
+U0(1:5,2) = 0;  % 5 infetti tra i giovani (fascia 19-50)
+U0(6:20,5) = 0; % 15 infetti tra gli adulti (50-70)
+U0(21:30,8) = 0; % 10 infetti tra gli anziani (70+)
+
 
 % parametri
-beta       = 0.5;
+beta       = 0.25;
 gamma      = 0.24;
 w          = 1/14;
-lambda1    = 0.1;
-lambda2    = 0.5;
-lambda3    = 0.9;
 t_lock     = 30;
 data       = cell(Tfin,1); 
 hbar       = waitbar(0,'','Name','Time iterations');
 flag       = 0;
+
+
+
 for t = 1:Tfin
     if (t>t_lock)&&(~flag)
-        M(1:3,1:3) = M(1:3,1:3)*lambda1;
-        M(4:6,4:6) = M(4:6,4:6)*lambda2;
-        M(7:9,7:9) = M(7:9,7:9)*lambda3;
-        flag       = 1;
+        M(1:3,1:3) = M(1:3,1:3) * 0.3;  % Giovani  %riduzioni fatte in base ad un crierio basato sul lockdown          M(4:6,4:6) = M(4:6,4:6) * 0.5;  % Adulti
+        M(7:9,7:9) = M(7:9,7:9) * 0.7;  % Anziani
+        beta=0.075;                     %il lockdown fa calare la possibilità di trasmissione
+        flag = 1;
     end
     for N_c = 1:numel(N_class)
         waitbar(N_c/numel(N_class),hbar,sprintf('Time step: %d/%d.\n $N_c$ = %d / %d',t,Tfin,N_c,numel(N_class)));
         % PRIMO PASSO
-        [f_new_tilda,U,num_bins,edges] = MonteCarlo(U0,beta,gamma,N_c,M(:,N_c));close all
+        [f_new_tilda,U,num_bins,edges] = MonteCarlo(t,U0,beta,gamma,N_c,M(:,N_c));close all
         % SECONDO PASSO
         f_new                          = PassoUpwind(L,num_bins,Tmax,CFL,w,f_new_tilda,3);
         % AGGIORNAMENTO
@@ -60,19 +63,23 @@ for t = 1:Tfin
             end
         end
         U0(1:N_class(N_c),N_c) = U; 
+        
         % CALCOLO S, I, R
         S=0;
         I=0;
         R=0;
-        for i=1:N_class(N_c)
-            if U0(i,N_c)<-1
-                S=S+1;
-            elseif abs(U0(i,N_c))<=1
-                I=I+1;
-            else
-                R=R+1;
+
+           for i = 1:N_class(N_c)
+                if U0(i, N_c) < -1
+                    S = S + 1;
+                elseif abs(U0(i, N_c)) <= 1
+                    I = I + 1;
+                else
+                    R = R + 1;
+                end
             end
-        end
+
+        
         T           = table(t, S, I, R, 'RowNames', {'Value'},...
                        'VariableNames', {'Time Step', 'Susceptible',...
                        'Infected', 'Removed'});
@@ -87,7 +94,8 @@ for t = 1:Tfin
 end
 close(hbar)
 % check popolazione conservata
-% check infetti
+% check infetti            %da qui in poi ho il dubbio che ci sia un
+% problema di calcolo
 Tot       = zeros(numel(N_class),1);
 confirmed = zeros(numel(N_class),1);
 removed   = zeros(numel(N_class),1);
